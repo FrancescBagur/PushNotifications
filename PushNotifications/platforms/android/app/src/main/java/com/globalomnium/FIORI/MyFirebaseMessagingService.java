7a8c18f7b267a9aa8d14db8dc643736ef2de0f03
FrancescBagur <bagur.francesc@gmail.com>
2018-08-09 14:54:34 +0200

    trabajo 09-08-2018

--- PushNotifications/platforms/android/app/src/main/java/com/globalomnium/FIORI/MyFirebaseMessagingService.java
+++ PushNotifications/platforms/android/app/src/main/java/com/globalomnium/FIORI/MyFirebaseMessagingService.java
@@ -1,19 +1,40 @@
 package com.globalomnium.FIORI;
 
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
 import android.app.Service;
+import android.content.Context;
 import android.content.Intent;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
 import android.os.IBinder;
 import android.support.annotation.Nullable;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
 
-public class MyFirebaseMessagingService extends Service {
+import com.firebase.jobdispatcher.Constraint;
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-    @Nullable
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
+    public static final String TAG = "NOTICIAS";
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        super.onMessageReceived(remoteMessage);
+
+        String from = remoteMessage.getFrom();
+        Log.d(TAG, "Mensaje recibido de: " +from);
+
+        if(remoteMessage.getNotification() != null){
+            Log.d(TAG, "Notificación: " + remoteMessage.getNotification().getBody());
+        }
     }
 }
