e4e5da8807d59deb00df17efd2f92320e2a7693b
MariaLazaroCh <42205493+MariaLazaroCh@users.noreply.github.com>
2018-08-09 14:50:49 +0200

    Firebase new version
    solucionado errores de version en el build gradle del firebase

--- PushNotifications/platforms/android/app/src/main/java/com/globalomnium/FIORI/MyFirebaseMessagingService.java
+++ PushNotifications/platforms/android/app/src/main/java/com/globalomnium/FIORI/MyFirebaseMessagingService.java
@@ -1,19 +1,67 @@
 package com.globalomnium.FIORI;
 
-import android.app.Service;
+import android.app.NotificationChannel;
+import android.app.NotificationManager;
+import android.app.PendingIntent;
+import android.content.Context;
 import android.content.Intent;
-import android.os.IBinder;
-import android.support.annotation.Nullable;
+import android.media.RingtoneManager;
+import android.net.Uri;
+import android.os.Build;
+import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
+import com.firebase.jobdispatcher.FirebaseJobDispatcher;
+import com.firebase.jobdispatcher.GooglePlayDriver;
+import com.firebase.jobdispatcher.Job;
 import com.google.firebase.iid.FirebaseInstanceId;
 import com.google.firebase.iid.FirebaseInstanceIdService;
+import com.google.firebase.messaging.FirebaseMessagingService;
+import com.google.firebase.messaging.RemoteMessage;
 
-public class MyFirebaseMessagingService extends Service {
+public class MyFirebaseMessagingService extends FirebaseMessagingService{
 
-    @Nullable
+    private static final String TAG = "MyFirebaseMsgService";
+
+    /**
+     * Called when message is received.
+     *
+     * @param remoteMessage Object representing the message received from Firebase Cloud Messaging.
+     */
+    // [START receive_message]
     @Override
-    public IBinder onBind(Intent intent) {
-        return null;
+    public void onMessageReceived(RemoteMessage remoteMessage) {
+        // [START_EXCLUDE]
+        // There are two types of messages data messages and notification messages. Data messages are handled
+        // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
+        // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
+        // is in the foreground. When the app is in the background an automatically generated notification is displayed.
+        // When the user taps on the notification they are returned to the app. Messages containing both notification
+        // and data payloads are treated as notification messages. The Firebase console always sends notification
+        // messages. For more see: https://firebase.google.com/docs/cloud-messaging/concept-options
+        // [END_EXCLUDE]
+
+
+        //"Title","Message","NotyType",   "hotelStatus"
+
+        String title = "";
+        if (remoteMessage.getNotification().getTitle() != null){
+            title = remoteMessage.getNotification().getTitle();
+        }
+
+        String message = "";
+        if (remoteMessage.getNotification().getBody() != null){
+            message = remoteMessage.getNotification().getBody();
+        }
+
+        Log.e("notification","recieved");
+
+
+         // Also if you intend on generating your own notifications as a result of a received FCM
+        // message, here is where that should be initiated. See sendNotification method below.
     }
+    // [END receive_message]
+
+
+
 }
