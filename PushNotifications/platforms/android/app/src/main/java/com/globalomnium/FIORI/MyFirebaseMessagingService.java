09498a8f9fc0b30e2fabd2c708ae8e26f1205688
MariaLazaroCh <42205493+MariaLazaroCh@users.noreply.github.com>
2018-08-10 08:32:42 +0200

    Cambios 09/08/2018
    Nueva version del Firebase
    Cambios clase Messaging

--- PushNotifications/platforms/android/app/src/main/java/com/globalomnium/FIORI/MyFirebaseMessagingService.java
+++ PushNotifications/platforms/android/app/src/main/java/com/globalomnium/FIORI/MyFirebaseMessagingService.java
@@ -1,25 +1,18 @@
 package com.globalomnium.FIORI;
 
-import android.app.NotificationChannel;
 import android.app.NotificationManager;
 import android.app.PendingIntent;
 import android.content.Context;
 import android.content.Intent;
-import android.media.RingtoneManager;
-import android.net.Uri;
-import android.os.Build;
-import android.support.v4.app.NotificationCompat;
 import android.util.Log;
 
 import com.firebase.jobdispatcher.FirebaseJobDispatcher;
 import com.firebase.jobdispatcher.GooglePlayDriver;
 import com.firebase.jobdispatcher.Job;
 import com.google.firebase.iid.FirebaseInstanceId;
-import com.google.firebase.iid.FirebaseInstanceIdService;
 import com.google.firebase.messaging.FirebaseMessagingService;
 import com.google.firebase.messaging.RemoteMessage;
-
-public class MyFirebaseMessagingService extends FirebaseMessagingService{
+public class MyFirebaseMessagingService extends FirebaseMessagingService {
 
     private static final String TAG = "MyFirebaseMsgService";
 
@@ -31,8 +24,6 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
     // [START receive_message]
     @Override
     public void onMessageReceived(RemoteMessage remoteMessage) {
-        // [START_EXCLUDE]
-        // There are two types of messages data messages and notification messages. Data messages are handled
         // here in onMessageReceived whether the app is in the foreground or background. Data messages are the type
         // traditionally used with GCM. Notification messages are only received here in onMessageReceived when the app
         // is in the foreground. When the app is in the background an automatically generated notification is displayed.
@@ -54,14 +45,7 @@ public class MyFirebaseMessagingService extends FirebaseMessagingService{
             message = remoteMessage.getNotification().getBody();
         }
 
-        Log.e("notification","recieved");
+        Log.e("notification", "recieved");
 
-
-         // Also if you intend on generating your own notifications as a result of a received FCM
-        // message, here is where that should be initiated. See sendNotification method below.
     }
-    // [END receive_message]
-
-
-
 }
